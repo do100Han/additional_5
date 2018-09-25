93c632afd7c30a35a8f45a193b6161db9519bd41
Александр Руткевич <a.rutkevich@webcom.lan>
2018-09-25 13:45:00 +0300

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  str = str.replace(/\s/g, "");
+  function bracketsSearch() {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      var item = bracketsConfig[i][0] + bracketsConfig[i][1];
+      var found = str.includes(item);
+      if (found !== false) {
+        deleteСoincidence(item);
+      }
+    }
+
+  }
+  function deleteСoincidence(item) {
+    str = str.replace(item, "");
+    bracketsSearch();
+  }
+
+  bracketsSearch();
+  if (str == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
